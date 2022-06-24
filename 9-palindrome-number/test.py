621619b26e0487e24e46e1e6549a4a2bcd335be2
DevSpade <rim9454@gmail.com>
2022-06-24 14:56:23 +0900

    palindrome solved

--- 9-palindrome-number/test.py
+++ 9-palindrome-number/test.py
@@ -1,8 +1,15 @@
 import re
 
 
-a = 12345
+a = 1243421
+#a = 111111
 rst = len(str(a))
 print(rst)
 
-reversed = string[::-1]
+reversed = str(a)[::-1]
+b = int(reversed)
+print(b)
+if a == b :
+    print("Pass")
+else :
+    print("Nope")
