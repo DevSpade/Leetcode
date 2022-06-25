e2d895794e55f14395b21487b452313751013568
DevSpade <rim9454@gmail.com>
2022-06-25 14:19:32 +0900

    [solved] 2-add-two-numbers

---
+++ test/linkedlist.py
@@ -0,0 +1,22 @@
+from this import d
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        print(l1,l2)
+        return None
+inputA = ListNode((2,3,4))
+inputB = ListNode((5,6,4))
+
+a = Solution()
+a.addTwoNumbers(inputA,inputB)
+#input = [2,3,4]
+
+#while input :
+ #   print(input)
