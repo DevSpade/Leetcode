41e0fede4e77a967372e8e7dae18a78909ceb7a3
DevSpade <rim9454@gmail.com>
2022-06-25 18:15:09 +0900

    add

--- test/linkedlist.py
+++ test/linkedlist.py
@@ -11,6 +11,12 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         print(l1,l2)
         return None
+
+inputA = [2,3,4]
+for value in inputA :
+    ListNode().val = value
+    ListNode().
+
 inputA = ListNode((2,3,4))
 inputB = ListNode((5,6,4))
 
