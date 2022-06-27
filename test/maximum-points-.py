3b4b403e06e14feffc630faffdb33f6bd033aa3e
DevSpade <rim9454@gmail.com>
2022-06-27 15:45:50 +0900

    [solved][1423] Maximum Points You Can Obtain from Cards

--- test/maximum-points-.py
+++ test/maximum-points-.py
@@ -6,15 +6,34 @@
 # 근데 그 경우에는 그 다음 숫자까지 고려할 수 없음 
 
 # 일단 k가 같은경우는 그냥 sum을하면됨
-
-from re import I
-
-
-class solution :
-    def maxScore ( self, cardPOints : List [int], k : int ) -> int :
+import types
+from typing import List
+class Solution() :
+    def maxScore ( self, cardPOints : List[int], k : int ) -> int :
         if k == len(cardPOints) :
             return sum(cardPOints)
-        
+        max_sum = 0
+        front = [0]
+        back = [0]
+        for i in range(k):
+            front.append(front[-1]+cardPOints[i])
+            back.append(back[-1]+cardPOints[-(i+1)])
+        print(front)
+        print(back)
+        for i in range(k+1):
+            max_sum = max(max_sum,front[i]+back[k-i])
+            print(max_sum)
+        return max_sum
 
-        
-        return 1
+a = Solution()
+rst = a.maxScore([1,9,3,4,5,6,1],k=4)
+#print(rst)
+
+"""
+b = [0,1]
+print(b[-1])
+b.append(b[-1]+10)
+b.append(b[-1]+11)
+b.append(b[-1]+12)
+print(b)
+"""
